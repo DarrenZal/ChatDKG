8c1a9a7a02039ed7c559695afd2b05e1fc55740b
root <zaldarren@gmail.com>
2023-12-24 23:32:13 +0000

    Remove TEST directory adn space-llm from repository

--- examples/langchain/TEST/NER.py
+++
@@ -1,7 +0,0 @@
-import spacy
-
-# Load a larger model
-nlp = spacy.load("en_core_web_md")  # or en_core_web_lg
-doc = nlp("Who is Monty?")
-for ent in doc.ents:
-    print(ent.text, ent.start_char, ent.end_char, ent.label_)
