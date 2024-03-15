b3966bd4e0117e36dfc0e51caa8bb038317070ce
root <zaldarren@gmail.com>
2024-03-15 05:01:23 +0000

    updates to dkg-demo and data

--- examples/langchain/dkg-demo.js
+++ examples/langchain/dkg-demo.js
@@ -5,16 +5,6 @@ import fs from "fs";
 // Parse the JSON data from file
 const jsonData = JSON.parse(fs.readFileSync("../utils/data.json"));
 
-/* // Initialize the DKG client on OriginTrail DKG Testnet
-const dkg = new DKG({
-  endpoint: process.env.OT_NODE_HOSTNAME,
-  blockchain: {
-    name: "otp::testnet",
-    publicKey: process.env.WALLET_PUBLIC_KEY,
-    privateKey: process.env.WALLET_PRIVATE_KEY,
-  },
-}); */
-
 const dkg = new DKG({
   endpoint: process.env.ENDPOINT_MAINNET,
   port: 8900,
@@ -39,26 +29,6 @@ async function createKnowledgeAsset(data) {
     console.error("Error creating Knowledge Asset:", error);
   }
 }
-
-
-// Main function to iterate over sections and create assets
-/* (async () => {
-  for (const section in jsonData) {
-    if (section != "blockchain_ecosystems_json_ld" 
-    && section != "profiles_json_ld"
-    && section != "organizations_json_ld"
-    && section != "countries_json_ld"
-    && section != "impact_areas_json_ld"
-    && section != "founderscircles_json_ld"
-    && section != "deals_json_ld"
-    ) {
-     console.log(section);
-     console.log(`Creating Knowledge Asset for section: ${section}`);
-     await createKnowledgeAsset(jsonData[section]);
-  }
-  }
-})(); */
-
 
 
 (async () => {
